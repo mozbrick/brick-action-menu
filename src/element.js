3d83e1d60057b0cb5e0f71d22f9b77d1a96fb95f
Les Orchard <me@lmorchard.com>
2014-07-28 16:14:38 -0400

    Stealing Action Menu style assets from Building Blocks; initial stab at element.js setup

--- src/element.js
+++ src/element.js
@@ -1,60 +1,88 @@
 (function () {
 
-  var ElementPrototype = Object.create(HTMLElement.prototype);
+  var BrickActionMenuElementPrototype = Object.create(HTMLElement.prototype);
+
+  var currScript = document._currentScript || document.currentScript;
+  var tmpl = currScript.ownerDocument.getElementById('action-menu-template');
+
+  // Attribute handlers
+ 
+  var attrs = {
+  };
+
+  function setup (element) {
+    element.ns = {};
+
+    // Grab root element from template, clone & remember it
+    var frag = document.importNode(tmpl.content, true);
+    element.root = frag.querySelector('form');
+
+    // Move all the custom element children into the cloned root
+    while (element.firstChild) {
+      element.root.appendChild(element.firstChild);
+    }
+
+    // Inject the cloned root into the document.
+    element.appendChild(element.root);
+  }
+
+  function update (element) {
+  }
 
   // Lifecycle methods
 
-  ElementPrototype.createdCallback = function () {
-
+  BrickActionMenuElementPrototype.createdCallback = function () {
+    setup(this);
   };
 
-  ElementPrototype.attachedCallback = function () {
-
+  BrickActionMenuElementPrototype.attachedCallback = function () {
+    for (var k in attrs) {
+      attrs[k].call(this, null, this.getAttribute(k));
+    }
+    update(this);
   };
 
-  ElementPrototype.detachedCallback = function () {
-
-  };
-
-  ElementPrototype.attributeChangedCallback = function (attr, oldVal, newVal) {
+  BrickActionMenuElementPrototype.attributeChangedCallback = function (attr, oldVal, newVal) {
     if (attr in attrs) {
       attrs[attr].call(this, oldVal, newVal);
     }
+    update(this);
+  };
+
+  BrickActionMenuElementPrototype.detachedCallback = function () {
+    // TODO: Do I really need to do this? Memory leak superstition.
+    this.root = this.ns = null;
   };
 
   // Custom methods
 
-  ElementPrototype.foo = function () {
-
+  /*
+  BrickActionMenuElementPrototype.foo = function () {
   };
+  */
 
-  // Attribute handlers
+  // Property handlers, magically boilerplated
 
-  var attrs = {
-    'attr': function (oldVal, newVal) {
-      oldVal = newVal = oldVal;
-
-    }
-  };
-
-  // Property handlers
-
-  Object.defineProperties(ElementPrototype, {
-    'prop': {
-      get : function () {
-
+  var props = {};
+  function makeProp (name) {
+    return {
+      get: function () {
+        return this.ns[name];
       },
-      set : function (newVal) {
-        newVal = null;
-
+      set: function (newVal) {
+        return this.attributeChangedCallback(name, this.ns[name], newVal);
       }
-    }
-  });
+    };
+  }
+  for (var name in attrs) {
+    props[name] = makeProp(name);
+  }
+  Object.defineProperties(BrickActionMenuElementPrototype, props);
 
   // Register the element
 
-  window.CustomElement = document.registerElement('brick-action-menu', {
-    prototype: ElementPrototype
+  window.BrickActionMenuElement = document.registerElement('brick-action-menu', {
+    prototype: BrickActionMenuElementPrototype
   });
 
 })();
