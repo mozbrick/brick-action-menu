df574914a7219f8c630d916ee8d38c170e8989d1
Les Orchard <me@lmorchard.com>
2014-07-31 14:12:39 -0400

    Add custom event and callback for show()

--- src/element.js
+++ src/element.js
@@ -5,8 +5,6 @@
   var currScript = document._currentScript || document.currentScript;
   var tmpl = currScript.ownerDocument.getElementById('action-menu-template');
 
-  var RE_BUTTON = /^button$/i;
-
   // Attribute handlers
  
   var attrs = {
@@ -14,32 +12,33 @@
 
   // Custom methods
   
-  BrickActionMenuElementPrototype.show = function (immediate) {
+  BrickActionMenuElementPrototype.show = function (callback, immediate) {
+    if (this.ns.shown) { return; }
+    this.ns.shown = true;
+    this.ns.callback = callback;
     this.root.classList.remove('fade-out');
     this.root.classList.remove('hide');
     this.root.classList.add(immediate ? 'show' : 'fade-in');
   };
 
   BrickActionMenuElementPrototype.hide = function (immediate) {
+    if (!this.ns.shown) { return; }
+    this.ns.shown = false;
+    this.ns.callback = null;
     this.root.classList.remove('fade-in');
     this.root.classList.remove('show');
     this.root.classList.add(immediate ? 'hide' : 'fade-out');
   };
 
-  /*
-  BrickActionMenuElementPrototype.foo = function () {
-  };
-  */
-
-  BrickActionMenuElementPrototype.foo = function () {
-    var _this = this;
-    this.root.set
-  };
+  var EV_PICK = 'pick';
+  var RE_BUTTON = /^button$/i;
 
   // Initial setup of the custom element
 
   function setup (_this) {
-    _this.ns = {};
+    _this.ns = {
+      shown: false
+    };
 
     // Grab root _this from template, clone & remember it
     var frag = document.importNode(tmpl.content, true);
@@ -50,18 +49,35 @@
       _this.root.appendChild(_this.firstChild);
     }
 
+    // Event delegation for all buttons in the menu
+    _this.root.addEventListener('click', function (e) {
+      if (!RE_BUTTON.test(e.target.tagName)) { return; }
+
+      // Do we have a callback supplied from show()?
+      if (_this.ns.callback) {
+        // HACK: Preserve the callback we're about to discard on hide(), but
+        // defer the callback for just a little bit.
+        (function (cb) {
+          setTimeout(function () { cb(e.target); }, 0.1);
+        })(_this.ns.callback);
+      }
+
+      // Dispatch a custom event for the menu item pick
+      e.target.dispatchEvent(new CustomEvent(EV_PICK, {
+        bubbles: true
+      }));
+
+      // Finish up by hiding the menu.
+      return _this.hide();
+    });
+
     // Squelch the form submission process
     _this.root.addEventListener('submit', function (e) {
       e.preventDefault();
       e.stopPropagation();
     });
 
-    _this.root.addEventListener('click', function (e) {
-      if (RE_BUTTON.test(e.target.tagName)) {
-        _this.hide();
-      }
-    });
-
+    // HACK: Annotate the menu for styling if there's a cancel button
     var elCancel = _this.root.querySelector('button.cancel');
     if (elCancel) {
       _this.root.setAttribute('data-with-cancel', 'true');
@@ -74,7 +90,7 @@
   // Post-setup update for custom element
 
   function update (_this) {
-    console.log("UPDATE " + element);
+    return _this;
   }
 
   function cleanup (_this) {
