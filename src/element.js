5995fc15a87f2d72b6ed7c8795d70c8ee5ff2732
Les Orchard <me@lmorchard.com>
2014-07-31 13:17:51 -0400

    Progress on examples and hide/show
    * More menu examples in index.html with buttons to show them
    
    * Methods to show/hide action menus
    
    * Styling tweaks to allow optional cancel button
    
    * Initial event handling for menu buttons

--- src/element.js
+++ src/element.js
@@ -5,40 +5,81 @@
   var currScript = document._currentScript || document.currentScript;
   var tmpl = currScript.ownerDocument.getElementById('action-menu-template');
 
+  var RE_BUTTON = /^button$/i;
+
   // Attribute handlers
  
   var attrs = {
   };
 
   // Custom methods
+  
+  BrickActionMenuElementPrototype.show = function (immediate) {
+    this.root.classList.remove('fade-out');
+    this.root.classList.remove('hide');
+    this.root.classList.add(immediate ? 'show' : 'fade-in');
+  };
+
+  BrickActionMenuElementPrototype.hide = function (immediate) {
+    this.root.classList.remove('fade-in');
+    this.root.classList.remove('show');
+    this.root.classList.add(immediate ? 'hide' : 'fade-out');
+  };
 
   /*
   BrickActionMenuElementPrototype.foo = function () {
   };
   */
 
+  BrickActionMenuElementPrototype.foo = function () {
+    var _this = this;
+    this.root.set
+  };
+
   // Initial setup of the custom element
 
-  function setup (element) {
-    element.ns = {};
+  function setup (_this) {
+    _this.ns = {};
 
-    // Grab root element from template, clone & remember it
+    // Grab root _this from template, clone & remember it
     var frag = document.importNode(tmpl.content, true);
-    element.root = frag.querySelector('form');
+    _this.root = frag.querySelector('form');
 
-    // Move all the custom element children into the cloned root
-    while (element.firstChild) {
-      element.root.appendChild(element.firstChild);
+    // Move all the custom _this children into the cloned root
+    while (_this.firstChild) {
+      _this.root.appendChild(_this.firstChild);
+    }
+
+    // Squelch the form submission process
+    _this.root.addEventListener('submit', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    });
+
+    _this.root.addEventListener('click', function (e) {
+      if (RE_BUTTON.test(e.target.tagName)) {
+        _this.hide();
+      }
+    });
+
+    var elCancel = _this.root.querySelector('button.cancel');
+    if (elCancel) {
+      _this.root.setAttribute('data-with-cancel', 'true');
     }
 
     // Inject the cloned root into the document.
-    element.appendChild(element.root);
+    _this.appendChild(_this.root);
   }
 
   // Post-setup update for custom element
 
-  function update (element) {
+  function update (_this) {
     console.log("UPDATE " + element);
+  }
+
+  function cleanup (_this) {
+    // TODO: Do I really need to do this? Memory leak superstition.
+    _this.root = _this.ns = null;
   }
 
   // Lifecycle methods
@@ -64,8 +105,7 @@
   };
 
   BrickActionMenuElementPrototype.detachedCallback = function () {
-    // TODO: Do I really need to do this? Memory leak superstition.
-    this.root = this.ns = null;
+    cleanup(this);
   };
 
   // Property handlers, magically boilerplated
