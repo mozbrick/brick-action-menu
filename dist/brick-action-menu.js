1a197a37a39d1875bd78efb9e0b9aebc6ed49e30
Les Orchard <me@lmorchard.com>
2014-08-06 17:30:47 -0400

    Update 2014-08-06T21:30:47.235Z

--- dist/brick-action-menu.js
+++ dist/brick-action-menu.js
@@ -6,42 +6,16 @@
 
   var BrickActionMenuElementPrototype = Object.create(HTMLElement.prototype);
 
-  // Element attribute handlers
-  var attrs = {
-
-    "visible": function (oldVal, newVal) {
-      // Be very particular about what values of "visible" mean not visible.
-      newVal = ('undefined' !== typeof newVal) &&
-               (null !== newVal) &&
-               (false !== newVal);
-
-      if (oldVal === newVal) {
-        return;
-      } else if (newVal === true) {
-        this.show();
-      } else if (newVal === false) {
-        this.hide();
-      }
-    }
-
-  };
-
   // Custom methods
 
   BrickActionMenuElementPrototype.show = function (callback) {
-    if (this.ns.visible) { return; }
-    this.ns.visible = true;
+    this.setAttribute('visible', true);
     this.ns.callback = callback;
-    this.classList.remove('hide');
-    this.classList.add('show');
   };
 
   BrickActionMenuElementPrototype.hide = function () {
-    if (!this.ns.visible) { return; }
-    this.ns.visible = false;
+    this.removeAttribute('visible');
     this.ns.callback = null;
-    this.classList.remove('show');
-    this.classList.add('hide');
   };
 
   var EV_PICK = 'pick';
@@ -51,6 +25,8 @@
 
   BrickActionMenuElementPrototype.createdCallback = function () {
 
+    this.ns = { };
+
     var importDoc = currentScript.ownerDocument;
     var templateContent = importDoc.querySelector('template').content;
 
@@ -59,16 +35,6 @@
     // create shadowRoot and append template to it.
     var shadowRoot = this.createShadowRoot();
     shadowRoot.appendChild(templateContent.cloneNode(true));
-
-    this.ns = {
-      visible: false
-    };
-
-    // HACK: Annotate the menu for styling if there's a cancel button
-    if (this.querySelector('button.cancel')) {
-      shadowRoot.querySelector('form')
-        .setAttribute('data-with-cancel', 'true');
-    }
 
     // Squelch the form submission process
     this.addEventListener('submit', function (e) {
@@ -82,6 +48,7 @@
 
       // Do we have a callback supplied from show()?
       if (this.ns.callback) {
+
         // HACK: Defer the callback, yielding for animations & etc.
         (function (cb) {
           setTimeout(function () { cb(e.target); }, 0.1);
@@ -89,6 +56,7 @@
 
         // Clear the callback (which is why we just used a closure to defer)
         this.ns.callback = null;
+
       }
 
       // Dispatch a custom event for the menu item pick
@@ -101,39 +69,9 @@
     });
   };
 
-  BrickActionMenuElementPrototype.attachedCallback = function () {
-    for (var k in attrs) {
-      if (this.hasAttribute(k)) {
-        attrs[k].call(this, undefined, this.getAttribute(k));
-      }
-    }
-  };
-
-  BrickActionMenuElementPrototype.attributeChangedCallback = function (attr, oldVal, newVal) {
-    if (attr in attrs) {
-      attrs[attr].call(this, oldVal, newVal);
-    }
-  };
-
   BrickActionMenuElementPrototype.detachedCallback = function () {
+    this.ns = null;
   };
-
-  // Property handlers, magically boilerplated from attribute handlers.
-  var props = {};
-  function makeProp (name) {
-    return {
-      get: function () {
-        return this.ns[name];
-      },
-      set: function (newVal) {
-        return this.attributeChangedCallback(name, this.ns[name], newVal);
-      }
-    };
-  }
-  for (var name in attrs) {
-    props[name] = makeProp(name);
-  }
-  Object.defineProperties(BrickActionMenuElementPrototype, props);
 
   // Register the element
   window.BrickActionMenuElement = document.registerElement('brick-action-menu', {
